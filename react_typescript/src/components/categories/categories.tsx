84245a535d5d44a167d4c960e025732cf2138f44
Robert Rochon <robert.rochon@codilime.com>
2019-02-12 15:03:56 +0100

    push

--- react_typescript/src/components/categories/categories.tsx
+++ react_typescript/src/components/categories/categories.tsx
@@ -1,25 +1,54 @@
 import * as React from "react";
-import { Badge } from "react-bootstrap";
+import { Badge, Col, Row } from "react-bootstrap";
 
-export interface ICategoriesProps {}
+export interface ICategoriesProps {
+  uniqueCategories: Array<any>;
+  uniqueBadges: Array<any>;
+}
 
 export default class ICategories extends React.Component<
   ICategoriesProps,
   any
 > {
   public render() {
+    const { uniqueBadges, uniqueCategories } = this.props;
+
     return (
-      <div>
-        <Badge pill className="categories-badge">
-          Primary
-        </Badge>
-        <Badge pill className="categories-badge">
-          Secondary
-        </Badge>
-        <Badge pill className="categories-badge">
-          Success
-        </Badge>
-      </div>
+      <>
+        {uniqueCategories.map(category => (
+          <Col className="justify-content-center" key={category}>
+            <div className="text-center">
+              <Badge pill className="categories-badge">
+                {category}
+              </Badge>
+            </div>
+            <div className="d-flex justify-content-center">
+              {uniqueBadges.map(badge => {
+                if (badge.cat === category) {
+                  return (
+                    <div className="text-center" key={badge.name}>
+                      <div
+                        className={`milestone-badge milestone-badge-${
+                          badge.cat
+                        }`}
+                      >
+                        {badge.name}
+                      </div>
+                      <div
+                        className={`milestone-rating milestone-rating-${
+                          badge.cat
+                        } d-flex align-items-center justify-content-center`}
+                      >
+                        <span>1</span>
+                      </div>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          </Col>
+        ))}
+      </>
     );
   }
 }
--- react_typescript/src/components/dashboard/dashboard.tsx
+++ react_typescript/src/components/dashboard/dashboard.tsx
@@ -4,32 +4,101 @@ import IDescription from "../description/description";
 import IProgressBar from "../progress-bar/prograss-bar";
 import ICategories from "../categories/categories";
 
-export interface IDashboardProps {}
+import { tracks } from "../../mockAPI/tracks";
 
-export default class IDashboard extends React.Component<IDashboardProps, any> {
+export interface IDashboardProps {
+  title?: string;
+}
+export interface IDashboardState {
+  userName: string;
+  skills: any[];
+}
+export default class IDashboard extends React.Component<
+  IDashboardProps,
+  IDashboardState
+> {
+  public static defaultProps: IDashboardProps = {
+    title: "Fake Snowflake"
+  };
+
+  public constructor(props: IDashboardProps) {
+    super(props);
+
+    this.state = {
+      userName: "Random User",
+      skills: []
+    };
+  }
+
+  private createSkillset = () => {
+    let allSkills: any[] = [];
+    let tracksKeys = Object.keys(tracks);
+
+    tracksKeys.forEach(key => {
+      console.log(tracks[key]);
+      allSkills.push(tracks[key]);
+    });
+
+    return allSkills;
+  };
+
+  public componentWillMount() {
+    let allSkills = this.createSkillset();
+    this.setState({
+      skills: [...allSkills]
+    });
+  }
+
   public render() {
+    const { title } = this.props;
+    const { userName, skills } = this.state;
+    // const createCategories = () => {
+    //   let categories = [];
+    //   for (let track in allTracks) {
+    //     categories.push(allTracks[track].category);
+    //   }
+
+    //   return categories.filter(
+    //     (value, index, categories) => categories.indexOf(value) === index
+    //   );
+    // };
+
+    // const uniqueCategories = createCategories();
+
+    // const createBadges = () => {
+    //   let badges = [];
+    //   for (let track in allTracks) {
+    //     let name = allTracks[track].displayName;
+    //     let cat = allTracks[track].category;
+    //     badges.push({ name, cat });
+    //   }
+    //   return badges;
+    // };
+
+    // const uniqueBadges = createBadges();
+
     return (
       <Container className="dashboard">
+        <Row>
+          <Col>{title}</Col>
+          <Col>{userName}</Col>
+        </Row>
         <Row className="py-4">
           <Col>
             <IProgressBar />
           </Col>
-          <Col>Snowflake</Col>
         </Row>
-        <Row className="py-4">
-          <Col>
-            <ICategories />
-          </Col>
+        <Row className="py-4 ">
+          <ICategories
+            uniqueCategories={uniqueCategories}
+            uniqueBadges={uniqueBadges}
+          />
         </Row>
         <Row className="py-4">
           <Col>
             <IDescription />
           </Col>
         </Row>
-        {/* //SLIDER
-    //SNOWFLAKE
-    //IMAGE
-    //RANGE */}
       </Container>
     );
   }
