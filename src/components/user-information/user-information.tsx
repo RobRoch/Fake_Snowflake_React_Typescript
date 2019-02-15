12c68742bf9ff2f487bd8477cb30b01ebe126d3e
Robert Rochon <robert.rochon@codilime.com>
2019-02-15 10:55:47 +0100

    push before typescript

--- src/components/user-information/user-information.tsx
+++ src/components/user-information/user-information.tsx
@@ -16,11 +16,11 @@ export default class IUserInformation extends React.Component<
         <h2>Your current position: {user.rank}</h2>
         <Row className="my-4">
           <Col className="d-flex flex-column align-items-center">
-            <h6 className="points-label mb-0">Current</h6>
+            <h6 className="points-label">Total points</h6>
             <Button className="points-button">{user.points}</Button>
           </Col>
           <Col className="d-flex flex-column align-items-center">
-            <h6 className="points-label mb-0">Next</h6>
+            <h6 className="points-label">Points to next level</h6>
             <Button className="points-button">
               {user.nextLevelPoints > 200 ? "Rly?" : user.nextLevelPoints}
             </Button>
